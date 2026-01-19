536bad20a45298a102d0d376fee9502512a5ce35
ConKirby <conckirby@gmail.com>
2026-01-19 15:02:45 +0000

    Plan

--- FlappyBird.java
+++ FlappyBird.java
@@ -1,3 +1,14 @@
+/* Plan:
+Create a method that displays a flappy bird game window in the console
+A method to shift the nested char arrays to the left by 1 position
+Method to add pipes to the right of the array
+Method to detect collisions with the pipes or the boundaries
+Method to count the score
+A method to handle the user input to move up or down
+A method to display the score and the game over message
+
+*/
+
 public class FlappyBird {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
